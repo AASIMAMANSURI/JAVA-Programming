42f1d158411a617f705ba5f46ebf9db95cc69d81
AASIMAMANSURI <136147114+AASIMAMANSURI@users.noreply.github.com>
2024-07-16 12:54:07 +0530

    Add files via upload

---
+++ Arethemetic_operaion.java
@@ -0,0 +1,53 @@
+	//1. Declare variables of different data types (int, double, boolean, char) and perform various
+	//arithmetic operations on them.
+	
+
+class Arethementic{
+
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 20;
+		float  aaa =10 ;
+		float bbb= 20;
+	
+		double aa=34.22;
+		double bb =099233.3;
+		
+		
+		System.out.print("interger:\n");
+		
+		int c = a+b;
+		System.out.println("addition " + c);
+		
+		int d = a-b;
+		System.out.println("subtraction " +d);
+		
+		int division = a/b ;
+		System.out.println("division" +division);
+		
+		int multiplication = a*b;
+		System.out.println("multiplication " + multiplication);
+		
+		int modulo =a %b ;
+		System.out.println("modulo" + modulo);
+		
+			System.out.print("float:\n ");
+		
+		float cc = a+b;
+		System.out.println("addition " + cc);
+		
+		float dd = a-b;
+		System.out.println("subtraction " +dd);
+		
+		float divisionf = a/b ;
+		System.out.println("division" +divisionf);
+		
+		float multiplicationf = a*b;
+		System.out.println("multiplication " + multiplicationf);
+		float modulof =a %b ;
+		System.out.println("modulo" + modulof);
+		
+	}
+	
+}
