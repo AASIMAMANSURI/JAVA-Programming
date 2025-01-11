a444f25b387f9c34521cadfce130ae6b514cf2fa
AASIMAMANSURI <136147114+AASIMAMANSURI@users.noreply.github.com>
2025-01-11 21:44:48 +0530

    Return the ASCII avlue of the character

---
+++ ASCII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Ascii{
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     
+     char ch ;
+     ch= input.next().charAt(0);
+
+int a= ch;
+System.out.println(a);
+    }
+}
